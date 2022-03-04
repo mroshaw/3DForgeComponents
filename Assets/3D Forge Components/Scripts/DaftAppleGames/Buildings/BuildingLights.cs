be3a9c66701dbf2a1c308bedc5a906cbb5c47f35
mroshaw <oli@mroshaw.co.uk>
2022-03-04 14:04:11 +0000

    Refactored for asset store and added lighting

---
+++ Assets/3D Forge Components/Scripts/DaftAppleGames/Buildings/BuildingLights.cs
@@ -0,0 +1,256 @@
+using System.Collections.Generic;
+using UnityEngine;
+#if HDPipeline
+using UnityEngine.Rendering.HighDefinition;
+#endif
+
+namespace DaftAppleGames.Buildings
+{
+    public class BuildingLights : MonoBehaviour
+    {
+        /// <summary>
+        /// Helper class to store list of lights and candle config
+        /// </summary>
+        private class LightConfig
+        {
+            private readonly GameObject _lightGameObject;
+            private GameObject _flameGameObject;
+            private readonly Light _light;
+            private int _index;
+            private bool _isCandle;
+
+            /// <summary>
+            /// Turn on light
+            /// </summary>
+            public void TurnOn()
+            {
+                _lightGameObject.SetActive(true);
+                if (_flameGameObject)
+                {
+                    _flameGameObject.SetActive(true);
+                }
+            }
+            
+            /// <summary>
+            /// Turn off light
+            /// </summary>
+            public void TurnOff()
+            {
+                _lightGameObject.SetActive(false);
+                if (_flameGameObject)
+                {
+                    _flameGameObject.SetActive(false);
+                }
+            }
+
+            public bool IsCandle()
+            {
+                return _isCandle;
+            }
+            
+            /// <summary>
+            /// Set the light state
+            /// </summary>
+            /// <param name="state"></param>
+            public void SetLightState(bool state)
+            {
+                _lightGameObject.SetActive(state);
+                if (_flameGameObject)
+                {
+                    _flameGameObject.SetActive(state);
+                }
+            }
+
+            public void ConfigureLightSource(float range, float intensity, float radius)
+            {
+                _light.range = range;
+                _light.intensity = intensity;
+#if HDPipeline
+                HDAdditionalLightData lightData = singleLight.gameObject.GetComponentInChildren<HDAdditionalLightData>();
+                if (lightData)
+                {
+                    lightData.shapeRadius = 3.0f;
+                }
+#else                
+#endif
+            }
+            
+            /// <summary>
+            /// Constructor
+            /// </summary>
+            /// <param name="index"></param>
+            /// <param name="lightGameObject"></param>
+            /// <param name="light"></param>
+            /// <param name="flameGameObject"></param>
+            /// <param name="isCandle"></param>
+            public LightConfig(int index, GameObject lightGameObject, Light light, GameObject flameGameObject, bool isCandle)
+            {
+                _index = index;
+                _lightGameObject = lightGameObject;
+                _light = light;
+                _isCandle = isCandle;
+            }
+            
+            /// <summary>
+            /// Simplified constructor
+            /// </summary>
+            /// <param name="index"></param>
+            /// <param name="lightGameObject"></param>
+            /// <param name="light"></param>
+            /// <param name="isCandle"></param>
+            public LightConfig(int index, GameObject lightGameObject, Light light, bool isCandle)
+            {
+                _index = index;
+                _lightGameObject = lightGameObject;
+                _light = light;
+                _isCandle = isCandle;
+            }
+            
+        }
+
+        [Header("Building light behaviour")]
+        [Tooltip("Set this for this buildings light config to override the global configuration.")]
+        public bool overrideGlobal;
+        [Tooltip("Set this to true to only impact candle light sources.")]
+        public bool candlesOnly;
+        [Tooltip("Set this if you want only child Light components to be effected. Leaving this blank will assume all lights within the parent Game Object to be effected.")]
+        public GameObject parentBuildingGameObject;
+        [Header("Light configuration")]
+        public float radius = 3;
+        public float intensity = 600;
+        public float range = 20;
+
+        
+        private bool _isLightsOn;
+        private List<LightConfig> _allLights;
+
+        /// <summary>
+        /// Configure all lights in the attached building object
+        /// </summary>
+        void Start()
+        {
+            ConfigureBuildingLights();
+        }
+    
+        /// <summary>
+        /// Turn on all lights
+        /// </summary>
+        public void TurnOnLights()
+        {
+            SetLightsState(true);
+            _isLightsOn = true;
+        }
+
+        /// <summary>
+        /// Turn off all lights
+        /// </summary>
+        public void TurnOffLights()
+        {
+            SetLightsState(false);
+            _isLightsOn = false;
+        }
+
+        /// <summary>
+        /// Toggle lights between on / off states
+        /// </summary>
+        public void ToggleLights()
+        {
+            SetLightsState(!_isLightsOn);
+        }
+        
+        /// <summary>
+        /// Sets the state of all lights in the building
+        /// </summary>
+        /// <param name="state"></param>
+        public void SetLightsState(bool state)
+        {
+            foreach (LightConfig singleLight in _allLights)
+            {
+                if (!candlesOnly || (candlesOnly && singleLight.IsCandle()))
+                {
+                    singleLight.SetLightState(state);
+                }
+            }
+            _isLightsOn = state;
+        }
+
+        /// <summary>
+        /// Configure all lights in the building with given properties
+        /// </summary>
+        public void ConfigureBuildingLights()
+        {
+            // Get all "Light" components from parent
+            Light[] allLightsInBuilding;
+            _allLights = new List<LightConfig>();
+            
+            if (parentBuildingGameObject)
+            {
+                allLightsInBuilding = parentBuildingGameObject.GetComponentsInChildren<Light>();
+            }
+            else
+            {
+                allLightsInBuilding = transform.parent.GetComponentsInChildren<Light>();
+            }
+
+            int currentIndex = 0;
+            // Iterate through lights and add to global building list
+            foreach (Light singleLight in allLightsInBuilding)
+            {
+                // Check if this is a candle
+                bool isCandle = singleLight.gameObject.name.ToLower().Contains("candle");
+                
+                // Look for the candle "Flame" particle GameObject
+                LightConfig newLightConfig;
+                if (isCandle)
+                {
+                    ParticleSystem flameParticle = singleLight.gameObject.transform.parent.gameObject
+                        .GetComponentInChildren<ParticleSystem>();
+                    if (flameParticle)
+                    {
+                        newLightConfig = new LightConfig(currentIndex, singleLight.transform.parent.gameObject,
+                            singleLight,
+                            flameParticle.gameObject, isCandle);
+                    }
+                    else
+                    {
+                        newLightConfig = new LightConfig(currentIndex, singleLight.transform.parent.gameObject,
+                            singleLight, isCandle);
+                    }
+                }
+                else
+                {
+                    newLightConfig = new LightConfig(currentIndex, singleLight.transform.parent.gameObject,
+                        singleLight, isCandle);
+                }
+
+                // Reconfigure light settings if overriding global controller settings
+                if (overrideGlobal)
+                {
+                    newLightConfig.ConfigureLightSource(range, intensity, radius);
+                }
+
+                // Add to building list
+                _allLights.Add(newLightConfig);
+                currentIndex++;
+            }
+            Debug.Log($"Configured: {_allLights.Count} lights");
+        }
+
+        /// <summary>
+        /// Apply new configuration for building lights and candles
+        /// </summary>
+        public void ReConfigureLightProperties(float newRange, float newIntensity, float newRadius)
+        {
+            if (!overrideGlobal)
+            {
+                foreach (LightConfig singleLight in _allLights)
+                {
+                    if (!candlesOnly || (candlesOnly && singleLight.IsCandle()))
+                    {
+                        singleLight.ConfigureLightSource(newRange, newIntensity, newRadius);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/3D Forge Components/Scripts/DaftAppleGames/Buildings/BuildingLightsController.cs
@@ -0,0 +1,135 @@
+using System;
+using System.Security.Cryptography;
+using UnityEngine;
+
+namespace DaftAppleGames.Buildings
+{
+    public class BuildingLightsController : MonoBehaviour
+    {
+        [Header("Global lighting overrides")]
+        [Tooltip("If unselected, all lights will use their own configuration. Lights that have override = TRUE will not be affected by this setting.")]
+        public bool reconfigureAllLights = true;
+        public float radius = 3;
+        public float intensity = 600;
+        public float range = 20;
+
+        [Header("Lights schedule")]
+        public int[] lightsOnHours;
+        public int[] lightsOffHours;
+
+        [Header("Debug")]
+        [Tooltip("Use this to toggle lights on and off for testing purposes")]
+        public KeyCode debugToggleLights = KeyCode.L;
+
+        private BuildingLights[] _allBuildingLights;
+        
+        // Useful debug
+        private bool _isLightsOn;
+        
+        /// <summary>
+        /// Determine current time and set initial light state
+        /// </summary>
+        public void Start()
+        {
+            // Get list of all lights
+            _allBuildingLights = FindObjectsOfType<BuildingLights>();
+            
+            // Configure using global settings. Note that any BuildingLights with Override = true,
+            // will not be re-configured
+            if (reconfigureAllLights)
+            {
+                ConfigureAllLights();
+            }
+            
+            // Default to Lights On
+            TurnOnLights();
+        }
+
+        /// <summary>
+        /// Configure all lights with Global configuration
+        /// </summary>
+        public void ConfigureAllLights()
+        {
+            if (reconfigureAllLights)
+            {
+                foreach (BuildingLights buildingLight in _allBuildingLights)
+                {
+                    buildingLight.ReConfigureLightProperties(radius, range, intensity);
+                }
+            }
+        }
+
+        /// <summary>
+        /// This public method can be called from an Event, such as Enviro's "OnHourPast" event.
+        /// that passes in the current hour. This method will evaluate whether the light state should change
+        /// and enact that change.
+        /// </summary>
+        /// <param name="hour"></param>
+        public void UpdateLightStatus(int hour)
+        {
+            if (Array.Exists(lightsOnHours, onHour => onHour == hour))
+            {
+                TurnOnLights();
+            }
+            
+            if (Array.Exists(lightsOffHours, offHour => offHour == hour))
+            {
+                TurnOffLights();
+            }
+        }
+        
+        /// <summary>
+        /// Turn on all building lights
+        /// </summary>
+        private void TurnOnLights()
+        {
+            SetAllBuildingLightsState(true);
+            _isLightsOn = true;
+        }
+
+        /// <summary>
+        /// Turn off all building lights
+        /// </summary>
+        private void TurnOffLights()
+        {
+            SetAllBuildingLightsState(false);
+            _isLightsOn = false;
+        }
+
+        /// <summary>
+        /// Toggle light state
+        /// </summary>
+        private void ToggleLights()
+        {
+            SetAllBuildingLightsState(!_isLightsOn);
+        }
+
+        /// <summary>
+        /// Set the state of all building lights
+        /// </summary>
+        /// <param name="state"></param>
+        private void SetAllBuildingLightsState(bool state)
+        {
+            // Debug.Log($"Updating {allLights.Length} buildings...");
+            foreach (BuildingLights buildingLight in _allBuildingLights)
+            {
+                buildingLight.SetLightsState(state);
+            }
+
+            _isLightsOn = state;
+        }
+
+        /// <summary>
+        /// Check for debug key press
+        /// </summary>
+        public void Update()
+        {
+            // Debug
+            if (Input.GetKeyDown(debugToggleLights))
+            {
+                // Debug.Log("Toggling lights...");
+                ToggleLights();
+            }
+        }
+    }
+}
